7f3d6a63e6173312120a275bbe6fadfd2285651e
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-18 13:34:17 -0700

    13 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+// console.log(returnFirstTwoDrivers());
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+// console.log(returnLastTwoDrivers());
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = function(multiplyFare) {
+    return function (fare) {
+        return multiplyFare * fare;
+    }
+}
+
+// function fareDoubler(fare) {
+//     return fare * 2;
+// }
+const fareDoubler = createFareMultiplier(2)
+
+// function fareTripler(fare) {
+//     return fare * 3;
+// }
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+}
